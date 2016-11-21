a0c0973fafa4d0a0c6dd943871903f64e826a3f2
skm2696 <skm03092014@gmail.com>
2016-11-21 17:13:07 +0400

    Update SortFile.h

--- include/SortFile.h
+++ include/SortFile.h
@@ -14,11 +14,11 @@ using namespace std;
 class SortFile
 {
 public:
-	SortFile(string name_main_file);
+	SortFile(string name_main_file, size_t buffer, string name_out_file);
 	auto file_size(string name_file)->size_t;
 	auto make_file(string name_file)->void;
 	auto sort()->void;
-	auto out_file(string line)->void;
+	auto out_file(string line, string name_out_file)->void;
 	auto remove_temp_files()->void;
 	~SortFile();
 	auto division() -> void;
