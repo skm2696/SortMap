7f281b21deaad3b4084cf3dd21fac74be17b1112
skm2696 <skm03092014@gmail.com>
2016-11-21 09:07:49 +0300

    Create Sort.h

---
+++ Sort.h
@@ -0,0 +1,33 @@
+#pragma once
+#include "stdafx.h"
+#ifndef SORT_HPP
+#define SORT_HPP
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+#include <map> 
+#include <iterator>
+#include <algorithm>
+#include <cstdio>
+using namespace std;
+class SortFile
+{
+public:
+	SortFile(string name_main_file);
+	auto file_size(string name_file)->size_t;
+	auto make_file(string name_file)->void;
+	auto sort()->void;
+	auto out_file(string line)->void;
+	auto remove_temp_files()->void;
+	~SortFile();
+private:
+	fstream file, f;
+	size_t buffer, count_of_files, closed_files;
+	bool out;
+	vector<string> lines;
+	vector<string> file_names;
+	multimap<string, size_t> map;
+};
+#include "SortFile.cpp"
+#endif
