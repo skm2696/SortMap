b16e1207d02ddd6642c2ca7d9a07487d1fe62f38
skm2696 <skm03092014@gmail.com>
2016-12-07 23:00:46 +0300

    Update init.cpp

--- tests/init.cpp
+++ tests/init.cpp
@@ -5,88 +5,36 @@
 #include <fstream>
 #include <chrono>
 using namespace std;
-SCENARIO("sort", "[sort]")
-{
-  auto begin = std::chrono::high_resolution_clock::now();
-  SortFile obj("small.txt", 1, "fl.txt");
-  auto end = std::chrono::high_resolution_clock::now();
-	auto res = std::chrono::duration_cast<std::chrono::milliseconds>(end - begin).count();
-	std::cout << "8 mb time: " << res << std::endl;
-//ifstream file("sort_small.txt");
- // ifstream out("fl.txt");
- bool p=true;
- //string s1,s2;
- //while (!file.eof()&&!out.eof()){
-// getline(file,s1);
-//  getline(out,s2);
- // if (s1==s2 || s1==""||s2==""){
-///  p=true;
- // }
-  // else
- //  {
-  //   p=false;
-  //   break;
- //  }
  
-// }
- // file.close();
- // out.close();
-  REQUIRE(p==true);
-}
-SCENARIO("sort2", "[sort2]")
-{
-	auto begin = std::chrono::high_resolution_clock::now();
-	SortFile obj("15.txt", 4, "fl2.txt");
-	auto res = std::chrono::duration_cast<std::chrono::milliseconds>(end - begin).count();
-	std::cout << "15 mb time: " << res << std::endl;
-//ifstream file2("sort_15.txt");
- // ifstream out2("fl2.txt");
+SCENARIO("32mb", "[32mb]"){
  bool p=true;
-// string s1,s2;
-//// while (!file2.eof()&&!out2.eof()){
-// getline(file2,s1);
- // getline(out2,s2);
- // if (s1==s2 || s1==""||s2==""){
- // p=true;
- // }
-//   else
-//   {
-   //  p=false;
-   //  break;
-  // }
+std::chrono::time_point<std::chrono::system_clock> start, end;
+	start = std::chrono::system_clock::now();
+ SortFile obj("32.txt",17, "out");
+ end = std::chrono::system_clock::now();
+	cout <<"32MB- " <<floor((end - start).count()/1000000000) <<" seconds"<< endl;
  
-// }
- // file2.close();
- // out2.close();
   REQUIRE(p==true);
 }
 
-SCENARIO("sort3", "[sort3]")
-{
-	auto begin = std::chrono::high_resolution_clock::now();
-	SortFile obj("32.txt", 17, "fl4.txt");
-	auto res = std::chrono::duration_cast<std::chrono::milliseconds>(end - begin).count();
-	std::cout << "32 mb time: " << res << std::endl;
-//ifstream file3("sort_32.txt");
- // ifstream out3("fl4.txt");
+SCENARIO("15mb", "[15mb]"){
  bool p=true;
-// string s1,s2;
-// long int i=0;
-// while (!file3.eof()&&!out3.eof()){
-// getline(file3,s1);
-//  getline(out3,s2);
- //  i++;
- // if (s1==s2 || s1==""|| s2=="")
-//  {
+std::chrono::time_point<std::chrono::system_clock> start, end;
+	start = std::chrono::system_clock::now();
+SortFile obj("15.txt",4, "out2");
+ end = std::chrono::system_clock::now();
+	cout <<"15MB- " <<floor((end - start).count()/1000000000) <<" seconds"<< endl;
  
-  //   p=true;
-     
- 
- 
-// }
-// }
- // file3.close();
- // out3.close();
   REQUIRE(p==true);
+}
 
+SCENARIO("8mb", "[8mb]"){
+bool p=true;
+std::chrono::time_point<std::chrono::system_clock> start, end;
+	start = std::chrono::system_clock::now();
+  SortFile obj("small.txt",1,"out8");
+ end = std::chrono::system_clock::now();
+	cout <<"8MB- " <<floor((end - start).count()/1000000000) <<" seconds"<< endl;
+ 
+  REQUIRE(p==true);
 }
