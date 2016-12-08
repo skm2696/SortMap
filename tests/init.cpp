f09db97d7e8b971e2cc224247198c183e7b6b15a
skm2696 <skm03092014@gmail.com>
2016-12-08 17:52:53 +0300

    Update init.cpp

--- tests/init.cpp
+++ tests/init.cpp
@@ -12,29 +12,29 @@ auto begin = std::chrono::high_resolution_clock::now();
  SortFile obj("32.txt",17, "out");
  auto end = std::chrono::high_resolution_clock::now();
 	auto res = std::chrono::duration_cast<std::chrono::milliseconds>(end - begin).count();
-	std::cout << "8 mb time: " << res << std::endl;
+	std::cout << "32 mb time: " << res << std::endl;
  
   REQUIRE(p==true);
 }
 
 SCENARIO("15mb", "[15mb]"){
  bool p=true;
-std::chrono::time_point<std::chrono::system_clock> start, end;
-	start = std::chrono::system_clock::now();
+auto begin = std::chrono::high_resolution_clock::now();
 SortFile obj("15.txt",4, "out2");
- end = std::chrono::system_clock::now();
-	cout <<"15MB- " <<floor((end - start).count()) <<" seconds"<< endl;
+auto end = std::chrono::high_resolution_clock::now();
+	auto res = std::chrono::duration_cast<std::chrono::milliseconds>(end - begin).count();
+	std::cout << "15 mb time: " << res << std::endl;
  
   REQUIRE(p==true);
 }
 
 SCENARIO("8mb", "[8mb]"){
 bool p=true;
-std::chrono::time_point<std::chrono::system_clock> start, end;
-	start = std::chrono::system_clock::now();
+auto begin = std::chrono::high_resolution_clock::now();
   SortFile obj("small.txt",1,"out8");
- end = std::chrono::system_clock::now();
-	cout <<"8MB- " <<floor((end - start).count()) <<" seconds"<< endl;
+auto end = std::chrono::high_resolution_clock::now();
+	auto res = std::chrono::duration_cast<std::chrono::milliseconds>(end - begin).count();
+	std::cout << "8 mb time: " << res << std::endl;
  
   REQUIRE(p==true);
 }
