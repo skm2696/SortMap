8aeb53d6e6a4b43608f99bb91fa528a9e0e495b3
skm2696 <skm03092014@gmail.com>
2016-12-08 13:51:06 +0300

    Update init.cpp

--- tests/init.cpp
+++ tests/init.cpp
@@ -12,7 +12,7 @@ std::chrono::time_point<std::chrono::system_clock> start, end;
 	start = std::chrono::system_clock::now();
  SortFile obj("32.txt",17, "out");
  end = std::chrono::system_clock::now();
-	cout <<"32MB- " <<floor((end - start).count()/1000000000) <<" seconds"<< endl;
+	cout <<"32MB- " <<floor((end - start).count()) <<" seconds"<< endl;
  
   REQUIRE(p==true);
 }
@@ -23,7 +23,7 @@ std::chrono::time_point<std::chrono::system_clock> start, end;
 	start = std::chrono::system_clock::now();
 SortFile obj("15.txt",4, "out2");
  end = std::chrono::system_clock::now();
-	cout <<"15MB- " <<floor((end - start).count()/1000000000) <<" seconds"<< endl;
+	cout <<"15MB- " <<floor((end - start).count()) <<" seconds"<< endl;
  
   REQUIRE(p==true);
 }
@@ -34,7 +34,7 @@ std::chrono::time_point<std::chrono::system_clock> start, end;
 	start = std::chrono::system_clock::now();
   SortFile obj("small.txt",1,"out8");
  end = std::chrono::system_clock::now();
-	cout <<"8MB- " <<floor((end - start).count()/1000000000) <<" seconds"<< endl;
+	cout <<"8MB- " <<floor((end - start).count()) <<" seconds"<< endl;
  
   REQUIRE(p==true);
 }
